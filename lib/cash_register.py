0b0fce4c89050903160d68af5c86ab32736822ec
Lee <leeirungu11313@gmail.com>
2024-05-29 09:06:49 +0300

    Python

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def __init__(self,discount= 0):
-    self.discount = discount
-    self.total = 0
-    self.items = []
-  def add_item(self,title,price,quantity=1):
-    self.items.append((title,price,quantity))
-    self.total += price * quantity
-  def apply_discount(self):
-      if self.discount > 0 :
-        self.total -= self.total * (self.discount / 100)
-      print(f"After the discount, the total comes to ${self.total}.")
+    def __init__(self, discount=0):
+        self.total = 0
+        self.items = []
+        self.discount = discount
+        self.last_transaction = 0  
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.last_transaction = price * quantity  
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            print(f"After the discount, the total comes to ${self.total:.0f}.") 
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.last_transaction:
+            self.total -= self.last_transaction
+            self.items.pop()
+            self.last_transaction = 0
+        else:
+            print("No transactions to void.")
